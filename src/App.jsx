998391bb26e04e41e2991ce41d54d73f53bb3e97
din-islam-rajin <din.islam.rajin572@gmail.com>
2023-04-13 22:54:53 +0600

    jobportal

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import "./App.css";
 import Banner from "./components/Pages/HomePage/Banner";
 import Categories from "./components/Pages/HomePage/Categories";
-import Navbar from "./components/Navbar"
+import Navbar from "./components/Navbar";
 import { RouterProvider } from "react-router-dom";
 import route from "../src/components/Router/Router.jsx";
 
-
 function App() {
-  return <>
+  return (
+    <>
       <RouterProvider router={route}></RouterProvider>
-  </>;
+    </>
+  );
 }
 
 export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -31,7 +31,6 @@ function Navbar() {
           Blog
         </li>
       </ul>
-
       <div className="bg-gradient-to-r from-[#7E90FE] to-[#9873FF] w-[194px] h-[64px] text-white flex justify-center items-center rounded-md font-bold">
         Start Applying
       </div>
